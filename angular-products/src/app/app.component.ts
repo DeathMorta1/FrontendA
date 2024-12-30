056d4b60e050b9454fc0b606340836acb3b55353
DeathMorta1 <capitanadri12@gmail.com>
2024-12-31 00:10:32 +0100

    añadimos interfaz y arreglamos components

--- angular-products/src/app/app.component.ts
+++ angular-products/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ProductsPageComponent } from './products-page/products-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ProductsPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
