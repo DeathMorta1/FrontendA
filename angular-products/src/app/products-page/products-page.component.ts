5618730eecc7e80610608beedcfbc815f4881a5c
DeathMorta1 <capitanadri12@gmail.com>
2024-12-31 00:14:06 +0100

    Creamos array de products y validamos que tenga datos

--- angular-products/src/app/products-page/products-page.component.ts
+++ angular-products/src/app/products-page/products-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../interfaces/product';
 
 @Component({
   selector: 'products-page',
@@ -7,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './products-page.component.css'
 })
 export class ProductsPageComponent {
-
+  title = "Mi lista de productos";
+  products: Product[] =[];
 }
