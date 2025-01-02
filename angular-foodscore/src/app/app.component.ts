5828ac520b2038d02fd61497b760cfec360c2c47
DeathMorta1 <capitanadri12@gmail.com>
2025-01-02 19:28:24 +0100

    añadimos el componente

--- angular-foodscore/src/app/app.component.ts
+++ angular-foodscore/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RestaurantsPageComponent } from './restaurants-page/restaurants-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RestaurantsPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular-foodscore/src/app/restaurants-page/restaurants-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'restaurants-page',
+  imports: [],
+  templateUrl: './restaurants-page.component.html',
+  styleUrl: './restaurants-page.component.css'
+})
+export class RestaurantsPageComponent {
+
+}
