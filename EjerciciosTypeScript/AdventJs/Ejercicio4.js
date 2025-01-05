8c680d8f12507a48519a8b3bebc5e20d98d9e6a3
DeathMorta1 <capitanadri12@gmail.com>
2025-01-05 20:13:07 +0100

    Ejercicio 4 a medias

---
+++ EjerciciosTypeScript/AdventJs/Ejercicio4.js
@@ -0,0 +1,19 @@
+function createXmasTree(height, ornament) {
+    const tree = (height*2)-1;
+    const padding = '_'.repeat(tree);
+    console.log(padding);
+    /* Code here */
+    return ''
+  }
+
+const tree = createXmasTree(5, '*')
+console.log(tree)
+/*
+____*____
+___***___
+__*****__
+_*******_
+*********
+____#____
+____#____
+*/
