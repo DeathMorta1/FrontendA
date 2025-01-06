eed24921e2c4b77ad472cc7f80b4ca6bd1e48a44
DeathMorta1 <capitanadri12@gmail.com>
2025-01-06 12:29:37 +0100

    Creacion Ejercicio5

---
+++ EjerciciosTypeScript/AdventJs/Ejercicio5.js
@@ -0,0 +1,14 @@
+const shoes = [
+    { type: 'I', size: 38 },
+    { type: 'R', size: 38 },
+    { type: 'R', size: 42 },
+    { type: 'I', size: 41 },
+    { type: 'I', size: 42 }
+];
+
+function organizeShoes(shoes) {
+    return []
+}
+  
+console.log(organizeShoes(shoes));
+// [38, 42]
