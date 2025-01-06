d28e1d7ca311609ee1704152ea0296ccd7240a62
DeathMorta1 <capitanadri12@gmail.com>
2025-01-06 13:28:04 +0100

    Ejercicio 5 terminado

--- EjerciciosTypeScript/AdventJs/Ejercicio5.js
+++ EjerciciosTypeScript/AdventJs/Ejercicio5.js
@@ -7,7 +7,48 @@ const shoes = [
 ];
 
 function organizeShoes(shoes) {
-    return []
+    const final=[];
+    const r = shoes.filter(p=> p.type==='R');
+    const i = shoes.filter(p=> p.type==='I');
+    if(r.length<i.length){
+        r.forEach(e => {
+            if(i.find(g=> g.size===e.size)){
+                final.push(e.size);
+            }
+        });
+    }else{
+        i.forEach(e => {
+            if(r.find(g=> g.size===e.size)){
+                final.push(e.size);
+            }
+        });
+    }
+    return final;
+}
+
+function organizeShoes1(shoes) {
+	const pairOfShoes = []
+	const iShoes = []
+	const rShoes = []
+
+	for (const shoe of shoes) {
+		if (shoe.type === 'I') {
+			iShoes.push(shoe.size)
+			continue
+		}
+
+		rShoes.push(shoe.size)
+	}
+
+	for (const iShoe of iShoes) {
+		const rShoeI = rShoes.findIndex((shoe) => shoe === iShoe)
+		if (rShoeI === -1) continue
+
+		pairOfShoes.push(iShoe)
+		rShoes.splice(rShoeI, 1)
+	}
+
+	return pairOfShoes
 }
   
 console.log(organizeShoes(shoes));
