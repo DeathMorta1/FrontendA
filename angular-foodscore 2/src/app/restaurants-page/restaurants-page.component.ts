f319949539b0afc180871f992cdedc59e2f48743
DeathMorta1 <capitanadri12@gmail.com>
2025-01-25 11:22:00 +0100

    Creamos foodscore2

---
+++ angular-foodscore 2/src/app/restaurants-page/restaurants-page.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { Restaurant } from '../interfaces/restaurant';
+import { FormsModule,NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'restaurants-page',
+  imports: [FormsModule],
+  templateUrl: './restaurants-page.component.html',
+  styleUrl: './restaurants-page.component.css'
+})
+
+export class RestaurantsPageComponent {
+  readonly days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  daysOpen: boolean[] = (new Array(7)).fill(true);
+  weekDay: number = new Date().getDay();
+  restaurants: Restaurant[] = [];
+
+  newRestaurant ={
+    name:'',
+    image:'',
+    cuisine:'',
+    description:'',
+    phone:'',
+    daysOpen:[] as string[]
+  };
+
+  changeImage(event: Event) {
+    const fileInput = event.target as HTMLInputElement;
+    if (!fileInput.files || fileInput.files.length === 0) { return; }
+    const reader: FileReader = new FileReader();
+    reader.readAsDataURL(fileInput.files[0]);
+    reader.addEventListener('loadend', () => {
+      this.newRestaurant.image = reader.result as string;
+    });
+  }
+
+  addRestaurant(form:NgForm){
+    this.newRestaurant.daysOpen = this.days.filter((p,i)=>this.daysOpen[i]===true);
+    this.restaurants.push({...this.newRestaurant});
+    form.resetForm();
+    this.newRestaurant.image = '';
+    this.daysOpen = (new Array(7)).fill(true);
+  }
+
+  removeRestaurant(i:number){
+    this.restaurants.splice(i,1);
+  }
+
+}
