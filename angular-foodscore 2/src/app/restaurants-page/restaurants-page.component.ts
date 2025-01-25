d015eabe31d3e70e571c4114b48072a6677abe94
DeathMorta1 <capitanadri12@gmail.com>
2025-01-26 00:43:16 +0100

    foodscore2 terminado

--- angular-foodscore 2/src/app/restaurants-page/restaurants-page.component.ts
+++ angular-foodscore 2/src/app/restaurants-page/restaurants-page.component.ts
@@ -1,28 +1,41 @@
-import { Component, signal} from '@angular/core';
+import { Component, computed, signal} from '@angular/core';
 import { Restaurant } from '../interfaces/restaurant';
 import { RestaurantFormComponent } from '../restaurant-form/restaurant-form.component';
 import { RestaurantCardComponent } from '../restaurant-card/restaurant-card.component';
+import { FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'restaurants-page',
-  imports: [RestaurantFormComponent,RestaurantCardComponent],
+  imports: [RestaurantFormComponent,RestaurantCardComponent,FormsModule],
   templateUrl: './restaurants-page.component.html',
   styleUrl: './restaurants-page.component.css'
 })
 
 export class RestaurantsPageComponent {
+  readonly days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  weekDay: number = new Date().getDay();
   restaurants = signal<Restaurant[]>([]);
 
-  addRestaurant(restaurant: Restaurant){
-    this.restaurants.update(restaurants=> restaurants.concat(restaurant));
-  }
+  search = signal('');
+  open = signal(true);
 
-  sendRest(){
-    this.restaurants.set([...this.restaurants()]);
+  restaurantsFiltered = computed(()=>{
+    const searchLower = this.search()?.toLowerCase();
+    return this.restaurants().filter(p=>
+        p.name.toLowerCase().includes(searchLower) ||
+        p.description.toLowerCase().includes(searchLower))
+  });
+
+  toggleRestaurants(){
+    this.open.update(o=>!o);
+  }
+  
+  addRestaurant(restaurant: Restaurant){
+    this.restaurants.update(restaurants=> [...restaurants,restaurant]);
   }
 
   deleteRestaurant(restaurant: Restaurant){
-    this.restaurants.update(restaurants=> restaurants.filter(p=> p!== restaurant));
+    this.restaurants.update(restaurants=> restaurants.filter(p=> p !== restaurant));
   }
 }
 
